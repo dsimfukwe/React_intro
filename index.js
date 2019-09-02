733c820e3764706ccd6546915ae210d69aca4bc1
dsimfukwe <dumisimfukwe@yahoo.com>
2019-09-02 21:31:51 +0100

    React_intro_updater_function
    incrementing state twice using updater function

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ class App extends React.Component {
     };
   }
   increaseCount() {
-    this.setState({ count: this.state.count + 1 });
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+    console.log(this.state.count);
   }
   render() {
     return (
