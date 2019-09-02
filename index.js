4bd6c5df735495d2b0a82eb1fa9fe6cd06d94d57
dsimfukwe <dumisimfukwe@yahoo.com>
2019-09-02 20:58:59 +0100

    React_intro_with_class
    React using class

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const styles = {
   textAlign: "center"
 };
 
-const App = props => (
-  <div style={styles}>
-    <h2>{props.count}</h2>
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div style={styles}>
+        <h2>{this.props.count}</h2>
+      </div>
+    );
+  }
+}
 let count = 0;
 setInterval(
   () => render(<App count={count++} />, document.getElementById("app")),
